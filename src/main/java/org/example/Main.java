f66e69e6b321204f6120ce91ab3ac1e71e3358a7
liliya komarova <liliya.komarova.86@mail.ru>
2023-01-06 13:01:32 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+import java.util.Iterator;
+
+public class Main {
+    public static void main(String[] args) {
+        Randoms randoms = new Randoms(90, 100);
+        Iterator iterator = randoms.iterator();
+        while (iterator.hasNext()) {
+            if (iterator.next().equals(100)) {
+                System.out.println("Выпало число 100");
+                break;
+            }
+            System.out.println(iterator.next());
+        }
+    }
+}
---
+++ src/main/java/org/example/Randoms.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+    protected Random random;
+    protected int min;
+    protected int max;
+    int randomNumber;
+
+
+    public Randoms(int min, int max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new RandomIterator();
+    }
+
+    private class RandomIterator implements Iterator {
+
+        @Override
+        public boolean hasNext() {
+            return true;
+        }
+
+        @Override
+        public Integer next() {
+            while (true) {
+                randomNumber = new Random().nextInt((max - min) + 1) + min;
+                return randomNumber;
+            }
+
+        }
+    }
+}
